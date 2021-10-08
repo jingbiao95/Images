f323203e16b25904d58ddcb61049a9d7ad24c166
jingbiao95 <jingbiao.nlp@gmail.com>
2021-10-08 10:40:57 +0800

    Upload by PicGo

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo yum install -y epel-release
+
+sudo yum install -y  lrzsz.x86_64
+sudo yum install -y nmap-ncat.x86_64
+sudo yum install -y net-tools
+sudo yum install -y vim-enhanced.x86_64
+sudo yum install -y sshpass
